128979d0a954df6b29886c0b8f59f6a44c004767
Dora Di <tdi@cphbusiness.dk>
2020-02-23 18:05:48 +0100

    role samples

--- docs/Scripts/getmyroles.sql
+++ docs/Scripts/getmyroles.sql
@@ -1,4 +1,4 @@
--- List of all user-defined roles
+-- A function to list of all user-defined roles
 CREATE OR REPLACE FUNCTION getmyroles()
 RETURNS TABLE (rolename name, memberof name[])
 AS $$
@@ -9,14 +9,21 @@ RETURN QUERY SELECT
             FROM pg_catalog.pg_auth_members m
             JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)
             WHERE m.member = r.oid) as memberof
-FROM pg_catalog.pg_roles r
-WHERE r.rolname NOT LIKE 'pg_%'
-ORDER BY 1;
+FROM pg_catalog.pg_roles r 			-- read all useres and roles
+WHERE r.rolname NOT LIKE 'pg_%'	   -- exclude system users and roles
+ORDER BY 1;						   -- sort by rolname
 END; $$ 
 LANGUAGE 'plpgsql';
 
+----------------------------------------------------------------------
 -- call the function
 SELECT * FROM getmyroles();
+SELECT * FROM getmyroles() where rolename = 'admin';
 
-SELECT * FROM getmyroles() where rolename = 'arne';
+----------------------------------------------------------------------
+-- See all roles and details
+-- SELECT rolname FROM pg_roles;
+SELECT * FROM pg_roles;
+
+
 
